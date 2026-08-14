77640030173efa1aba1fe9cb30c043be002f8564
UNDAMAG3D <k.tarazi@yahoo.com>
2026-08-14 21:02:28 +0300

    Add clsBankClient class and client search functionality
    Introduced the `clsBankClient` class with properties for account
    management, including account number, pin code, and balance.
    Added methods to check if a client object is empty, retrieve
    account details, and print client information.
    
    Implemented static `find()` methods to search for clients by
    account number or account number and pin code. Updated
    `bank-system.cpp` to include `clsBankClient` and demonstrate
    client search functionality. Added placeholders for future
    client data storage and retrieval using a vector.

--- bank-system.cpp
+++ bank-system.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-
+#include "clsPerson.h"
+#include "clsBankClient.h"
 using namespace std;
 
 int main() {
-    
+     
+    //I need to find the client without creating object using find(), if I found it I return object (client card) with client info, if not I return empty (client card) object.
+    clsBankClient client1 = clsBankClient::find("A101");
+    client1.print();
+
+    clsBankClient client2 = clsBankClient::find("A101", "1234");
+
+    system("pause>0");
+    return 0;
 }
--- clsBankClient.h
+++ clsBankClient.h
@@ -9,6 +9,91 @@ using namespace std;
 
 class clsBankClient : public clsPerson {
 
+private:
+
+	enum enMode { emptyMode = 0, updateMode = 1};
+	enMode _Mode;
+
+	string _AccountNumber;
+	string _PinCode;
+	float _AccountBalance;
+
+
+public:
+
+	clsBankClient(enMode mode, string firstName, string lastName, string email, string phone, 
+		string accountNumber, string pinCode, float accountBalance) : clsPerson(firstName, lastName, email, phone) {
+		
+		_Mode = mode;
+		_AccountNumber = accountNumber;
+		_PinCode = pinCode;
+		_AccountBalance = accountBalance;
+	}
+
+	bool isEmpty() {	//to check the mode is empty or not
+
+		return (_Mode == enMode::emptyMode);
+	}
+
+	string accountNumber() { // read only property, no one is allowed to edit it
+		
+		return _AccountNumber;
+	}
+
+	void setPinCode(string pinCode) {
+		
+		_PinCode = pinCode;
+	}
+
+	string getPinCode() {
+
+		return _PinCode;
+	}
+
+	__declspec(property(get = getPinCode, put = setPinCode)) string pinCode;
+
+	void setAccountBalance(float accountBalance) {
+
+		_AccountBalance = accountBalance;
+	}
+
+	float getAccountBalance() {
+
+		return _AccountBalance;
+	}
+
+	__declspec(property(get = getAccountBalance, put = setAccountBalance)) float accountBalance;
+
+	void print() {
+
+		cout << "\nClient Card:";
+		cout << "\n___________________";
+		cout << "\nFirstName   : " << firstName;
+		cout << "\nLastName    : " << lastName;
+		cout << "\nFull Name   : " << fullName();
+		cout << "\nEmail       : " << email;
+		cout << "\nPhone       : " << phone;
+		cout << "\nAcc. Number : " << _AccountNumber;
+		cout << "\nPassword    : " << _PinCode;
+		cout << "\nBalance     : " << _AccountBalance;
+		cout << "\n___________________\n";
+	}
+
+	static clsBankClient find(string accountNumber) {
+		
+		vector<clsBankClient> vClients; // structure and it's type the class itself
+
+	}
+
+	static clsBankClient find(string accountNumber, string pinCode) {
+
+	}
+
+
+
+
+
+
 
 
 };
