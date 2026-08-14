3fb57f5176ed5762cf540eba5ad649fff1354aae
UNDAMAG3D <k.tarazi@yahoo.com>
2026-08-14 05:30:54 +0300

    Add project files.

---
+++ clsBankClient.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "clsPerson.h"
+#include "clsString.h"
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class clsBankClient : public clsPerson {
+
+
+
+};
+
