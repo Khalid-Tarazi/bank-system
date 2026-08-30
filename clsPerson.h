0a1d724ff104a1281a018aba9edb051de738e6c1
UNDAMAG3D <k.tarazi@yahoo.com>
2026-08-31 01:06:34 +0300

    Add InterfaceCommunication interface and integrate clsPerson
    Introduced a new `InterfaceCommunication` interface with methods for sending email, fax, and SMS. Updated `clsPerson` to inherit from this interface and added empty implementations for the required methods. Included `InterfaceCommunication.h` in the project and updated project filters to categorize it under "Header Files."

--- clsPerson.h
+++ clsPerson.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "InterfaceCommunication.h"
 using namespace std;
 
-class clsPerson {
+class clsPerson: public InterfaceCommunication {
 
 private:
 
@@ -65,5 +66,17 @@ public:
 	string fullName() {
 		
 		return _FirstName + " " + _LastName;
+	}
+
+	void SendEmail(string Title, string Body) {
+
+	}
+
+	void SendFax(string Title, string Body) {
+
+	}
+
+	void SendSMS(string Title, string Body) {
+
 	}
 };
